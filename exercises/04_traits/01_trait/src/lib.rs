4c70f4bbba385431a8cc9455e67edee4f2f7dd23
eggsNBakey <beeman134@gmail.com>
2024-12-25 21:00:24 -0600

    quick update
    not much changed just advanced 5 modules

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -3,6 +3,22 @@
 //
 // Then implement the trait for `u32` and `i32`.
 
+pub trait is_even{
+    fn is_even(&self) -> bool;
+}
+
+impl is_even for u32{
+    fn is_even(&self)->bool{
+        self%2==0
+    }
+}
+
+impl is_even for i32{
+    fn is_even(&self)->bool{
+        self%2==0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
